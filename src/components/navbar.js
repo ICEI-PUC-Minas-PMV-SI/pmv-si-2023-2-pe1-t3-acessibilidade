d0d1796a7f77e0825f479c5327a8ba3c048e1bae
Ana Cecilia <113299281+cecipsc@users.noreply.github.com>
2023-10-28 18:59:55 -0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -48,32 +48,27 @@ const navbarComponent = `
             </svg>
             <a class="nav-link" href="#"><span id="nav-menu">Cadastrar local</span></a>
           </li>
-          <li class="nav-item d-flex justify-items-center align-items-center" role="menuitem">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users-group" width="24"
-              height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-              stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-              <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1"></path>
-              <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-              <path d="M17 10h2a2 2 0 0 1 2 2v1"></path>
-              <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-              <path d="M3 13v-1a2 2 0 0 1 2 -2h2"></path>
-            </svg>
-            <a class="nav-link" href="#"><span id="nav-menu">Quem somos</span></a>
-          </li>
-          <li class="nav-item d-flex justify-items-center align-items-center" role="menuitem">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-info-circle" width="24"
-              height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-              stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-              <path d="M12 9h.01"></path>
-              <path d="M11 12h1v4h1"></path>
-            </svg>
-            <a class="nav-link" href="#"><span id="nav-menu">Informação</span></a>
-          </li>
-        </ul>
+          
+          <li class="nav-item dropdown ">
+              <a class="d-flex justify-items-center align-items-center nav-link dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <svg xmlns="http://www.w3.org/2000/svg" class="me-2 icon icon-tabler icon-tabler-info-circle" width="24"
+                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                <path d="M12 9h.01"></path>
+                <path d="M11 12h1v4h1"></path>
+              </svg>
+                Informação
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Sobre a iniciativa</a></li>
+                <li><a class="dropdown-item" href="#">Sobre acessibilidade</a></li>
+                <li><a class="dropdown-item" href="tiposA11y.html">Tipos de acessibilidade</a></li>
+                <li><a class="dropdown-item" href="#">Como funciona</a></li>
+              </ul>
+            </li>
+          </ul>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Local" aria-label="Search">
           <button class="btn btn-outline-primary" type="submit">Buscar</button>
