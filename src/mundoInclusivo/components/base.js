a6da62e55396c442806e2fcb2e83b55caa83e2ef
Mari <mareaceae@gmail.com>
2023-11-26 18:13:26 -0300

    fix: login user and estab

--- src/mundoInclusivo/components/base.js
+++ src/mundoInclusivo/components/base.js
@@ -1,46 +1,67 @@
 const getEstabLoginForm = () => document.getElementById('estabLoginForm');
 const getEstabSigninModal = () => document.getElementById('estabFormSigninModal');
+const getUserLoginForm = () => document.getElementById('userFormLogin');
+const getUserSigninModal = () => document.getElementById('estabFormSigninModal');
+
 const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 
 const LOGIN_INPUT_KEYS = {
-  estabEmail: "Email Estabelecimento",
-  estabPass: "Senha"
+  email: "Email",
+  pass: "Senha"
+}
+
+const setLoginData = (ev) => {
+  ev.preventDefault();
+
+  let currentForm = ev.target;
+  console.log
+  let inputs = currentForm.querySelectorAll('input')
+
+  let currentLogin = {}
+  
+  for (const input of inputs) {
+    let key = LOGIN_INPUT_KEYS[input.id]
+    currentLogin[key] = input.value
+  }
+
+  return currentLogin
+}
+
+const verifyLogin = (base, loginData, callback) => {;
+  let currentLogin = base.find(stores => {
+    let loginKey = LOGIN_INPUT_KEYS['email'];
+    return stores[loginKey] === loginData[loginKey]
+  })
+
+  let loginPassowrdKey = LOGIN_INPUT_KEYS['pass']
+  let storePassword = currentLogin?.[loginPassowrdKey]
+  let currentPassword = loginData?.[loginPassowrdKey]
+
+  let isSamePassword = storePassword === currentPassword
+
+  if(isSamePassword) {
+    callback(currentLogin)
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   let estabLoginForm = getEstabLoginForm();
   let estabSigninFormModal = getEstabSigninModal();
+  let userLoginForm = getUserLoginForm();
 
   estabLoginForm.addEventListener('submit', ev => {
-    ev.preventDefault();
-
-    let currentForm = ev.target;
-    let inputs = currentForm.querySelectorAll('input')
-   
-    let currentLogin = {}
-    
-    for (const input of inputs) {
-      let key = LOGIN_INPUT_KEYS[input.id]
-      currentLogin[key] = input.value
-    }
-
+    let currentLogin = setLoginData(ev)
     let currentStores = window.getStores();
-    let currentStoreLogin = currentStores.find(stores => {
-      let loginKey = LOGIN_INPUT_KEYS['estabEmail'];
-      return stores[loginKey] === currentLogin[loginKey]
-    })
 
-    let loginPassowrdKey = LOGIN_INPUT_KEYS['estabPass']
-    let storePassword = currentStoreLogin?.[loginPassowrdKey]
-    let currentPassword = currentLogin?.[loginPassowrdKey]
+    verifyLogin(currentStores, currentLogin, window.loginStore)
+  })
 
-    let isSamePassword = storePassword === currentPassword
+  userLoginForm.addEventListener('submit', ev => {
+    let currentLogin = setLoginData(ev)
+    let currentUsers = window.getUsers();
 
-    if(isSamePassword) {
-      window.loginStore(currentStoreLogin)
-    }
-
-})
+    verifyLogin(currentUsers, currentLogin, window.loginUser)
+  })
 
   estabSigninFormModal.addEventListener('submit',ev=>{
       ev.preventDefault();
--- src/mundoInclusivo/components/local-storage.js
+++ src/mundoInclusivo/components/local-storage.js
@@ -2,7 +2,7 @@ const BASE_STORES = [{
   "Nome do Proprietário": "Mariana Mattos",
   "Razão Social": "Mari Software House",
   "CNPJ": "173.017.447-73",
-  "Email Estabelecimento": "marianasmmattos@gmail.com",
+  "Email": "marianasmmattos@gmail.com",
   // TODO: VOLTAR PRA VER
   // TODO: Pensar se precisa de um ID
   // ENDEREÇO: {
@@ -10,53 +10,98 @@ const BASE_STORES = [{
   //  }
   "Endereço": "Rua Silva Teles",
   "Telefone": "21996903276",
-  "Senha": "12345678"
+  "Senha": "12345678",
+  "Tipos de deficiência": [],
+  "Foto": ''
+}]
+
+const BASE_USERS = [{
+  "Nome completo": "Mariana Mattos",
+  "Email": "marianasmmattos@gmail.com",
+    // TODO: VOLTAR PRA VER
+  // TODO: Pensar se precisa de um ID
+  // ENDEREÇO: {
+  //    maps: fvdfbfd
+  //  }
+  "Endereço": "Rua Silva Teles",
+  "Telefone": "21996903276",
+  "Senha": "12345678",
+  "Tipos de deficiência": [],
+  "Foto": ''
 }]
 
 const LOCAL_STORES_KEY = 'locaisCadastrados';
 const STORE_SESSION_KEY = 'localLogado';
+const LOCAL_USERS_KEY = 'usuariosCadastrados';
+const USER_SESSION_KEY = 'usuarioLogado';
 
-const globalLocalStorage = window.localStorage
+const globalLocalStorage = window.localStorage;
+const getStoreItem = (key) => {
+  let values = globalLocalStorage.getItem(key);
+  return JSON.parse(values)
+}
 
-const cleanStorage = () => {
-  globalLocalStorage.setItem(LOCAL_STORES_KEY, '')
+const setStoreItem = (key, values) => {
+  let stringifiedJson = JSON.stringify(values)
+  globalLocalStorage.setItem(key, stringifiedJson);
+  return values
 }
 
 const getStores = () => {
-  let currentStores = globalLocalStorage.getItem(LOCAL_STORES_KEY);
-  let parsedStores = JSON.parse(currentStores)
-
+  let parsedStores = getStoreItem(LOCAL_STORES_KEY);
   return parsedStores?.locais
 }
 
 const saveStores = (store) => {
   let currentStores = getStores()?.locais || [];
   let newStores = [...BASE_STORES, ...currentStores, store]
-  let stringifiedJson = JSON.stringify({ locais: newStores })
-
-  globalLocalStorage.setItem(LOCAL_STORES_KEY, stringifiedJson);
+  
+  setStoreItem(LOCAL_STORES_KEY, { locais: newStores })
 
   return store
 }
 
 const loginStore = (store) => {
-  let stringifiedJson = JSON.stringify(store)
-  globalLocalStorage.setItem(STORE_SESSION_KEY, stringifiedJson)
-
+  setStoreItem(STORE_SESSION_KEY, store)
   return store
 }
 
 const getSessionStore = () => {
-  let currentStore = globalLocalStorage.getItem(STORE_SESSION_KEY)
-  let parsedStore = JSON.parse(currentStore)
-
+  let parsedStore = getStoreItem(STORE_SESSION_KEY)
   return parsedStore
+}
+
+const loginUser = (user) => {
+  setStoreItem(USER_SESSION_KEY, user)
+  return user
+}
+
+const getUsers = () => {
+  let parsedUsers = getStoreItem(LOCAL_USERS_KEY);
+  return parsedUsers?.users
+}
+
+const saveUsers = (user) => {
+  let currentUsers = getStores()?.locais || [];
+  let newUsers = [...BASE_USERS, ...currentUsers, user]
+  
+  setStoreItem(LOCAL_USERS_KEY, { users: newUsers })
+
+  return user
 }
 
 window.onload = function() {
   window.saveStores = saveStores
   window.getStores = getStores
+
   window.loginStore = loginStore
   window.getSessionStore = getSessionStore
+
+  window.saveUsers = saveUsers
+  window.getUsers = getUsers
+
+  window.loginUser = loginUser
+
   window.saveStores({})
+  window.saveUsers({})
 }
